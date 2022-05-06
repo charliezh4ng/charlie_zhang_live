77489e9dbea0248f6c1002648d6cda8cd628fa8b
Charles <charles.at.the.gym@gmail.com>
2022-05-06 08:07:54 -0400

    rig create comment to email me

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -2,7 +2,8 @@ class CommentsController < ApplicationController
   before_action :set_post
 
   def create
-    @post.comments.create! params.required(:comment).permit(:content)
+    comment = @post.comments.create! params.required(:comment).permit(:content)
+    CommentsMailer.submitted(comment).deliver_later
     redirect_to @post
   end
 
