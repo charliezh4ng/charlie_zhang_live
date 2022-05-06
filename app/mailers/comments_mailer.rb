fe49afacdd27111b4650257daee2dbe3a85e5f19
Charles <charles.at.the.gym@gmail.com>
2022-05-06 08:05:29 -0400

    add mailer to update me new comments

---
+++ app/mailers/comments_mailer.rb
@@ -0,0 +1,13 @@
+class CommentsMailer < ApplicationMailer
+
+  # Subject can be set in your I18n file at config/locales/en.yml
+  # with the following lookup:
+  #
+  #   en.comments_mailer.submitted.subject
+  #
+  def submitted(comment)
+    @comment = comment
+
+    mail to: "me@example.org", subject: "New comment :D"
+  end
+end
